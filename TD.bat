c6cc3eb2992a76f12e97895715f1f519e9a75963
vavavr00m <vavavr00m@users.noreply.github.com>
2023-10-06 14:51:56 +0800

    Update TD.bat

--- TD.bat
+++ TD.bat
@@ -1,32 +1,38 @@
 @echo off
 
+set drive_letter=c d e f g h i j k l m n o p q r s t u v w x y z
 set "commons=steamapps\common\Conan Exiles"
-set "specific=ConanSandbox\Content\Movies"
 set ext=mp4 mov mkv m4p m4v webm flv f4v f4p f4a f4b vob ogv drc gifv mng avi mts m2ts ts qt wmv yuv rm rmvb viv asf amv mpg mp2 mpeg mpe mpv m2v svi 3gp 3g2 mxf roq nsv rv cpk dvr-ms wtv
-
-set loc="D:\SteamLibrary\%commons%\%specific%" "E:\SteamLibrary\%commons%\%specific%" "D:\Games\%commons%\%specific%" "G:\Steam\%commons%\%specific%" "C:\Program Files (x86)\Steam\%commons%\%specific%"
-
-echo\
-for %%A in (%loc%) do (
-	if exist "%%~A" ( echo Exists - "%%~A" && echo\ && if not exist "%%~A\bak" ( md "%%~A\bak" ) else ( 
-			for %%B in (%%A\*) do if not exist "%%~A\%%~nB" ( echo Creating a blank file - %%~nB && break>"%%~A\%%~nB" ) else ( echo Exists - "%%~A\%%~nB" )
-			for %%C in (%ext%) do if exist "%%~A\*.%%C" ( echo\ && echo Processing all %%C files in %%A && echo\ && dir "%%~A\*.%%C" && echo\ && echo Moving the following to "%%~A\bak" && @move "%%~A\*.%%C" "%%~A\bak" && echo\ ) else ( echo No file with %%C extension exists in "%%~A" )
-		) 
-	) else ( echo\ && echo Does not exist - "%%~A")
-)
-goto :replace_DefaultGame_ini
+set "specific=ConanSandbox\Content\Movies"
+set "ini=ConanSandbox\Config\DefaultGame.ini"
+goto :process
 exit /b 0
 
-:replace_DefaultGame_ini
-echo\ && echo Setting the location..
-set "specific=ConanSandbox\Config\DefaultGame.ini"
-set loc="D:\SteamLibrary\%commons%\%specific%" "E:\SteamLibrary\%commons%\%specific%" "D:\Games\%commons%\%specific%" "G:\Steam\%commons%\%specific%" "C:\Program Files (x86)\Steam\%commons%\%specific%"
-
+:process
 echo\
-for %%A in (%loc%) do (
-	if exist "%%~A" ( echo Exists - "%%~A" && start "" /wait /min powershell.exe -NoP -Ex -By -c "(Get-Content -Path '%%~A') | ForEach-Object { $_.Replace(';+StartupMovies=StartupUE4','').Replace(';+StartupMovies=StartupNvidia','').Replace(';+StartupMovies=CinematicIntroV2','').Replace(';-StartupMovies=StartupUE4','').Replace(';-StartupMovies=StartupNvidia','').Replace(';-StartupMovies=CinematicIntroV2','').Replace('; -StartupMovies=StartupUE4','').Replace('; -StartupMovies=StartupNvidia','').Replace('; -StartupMovies=CinematicIntroV2','').Replace('; +StartupMovies=StartupUE4','').Replace('; +StartupMovies=StartupNvidia','').Replace('; +StartupMovies=CinematicIntroV2','').Replace('+StartupMovies=StartupUE4','').Replace('+StartupMovies=StartupNvidia','').Replace('+StartupMovies=CinematicIntroV2','').Replace('-StartupMovies=StartupUE4','').Replace('-StartupMovies=StartupNvidia','').Replace('-StartupMovies=CinematicIntroV2','').Replace('bWaitForMoviesToComplete=True','bWaitForMoviesToComplete=False').Replace('bMoviesAreSkippable=False','bMoviesAreSkippable=True').Replace('-StartupMovies=','').Replace(';-StartupMovies=','').Replace('; -StartupMovies=','') } | Set-Content -Path '%%~A'" ) else ( echo Does not exist - "%%~A" )
+for %%A in (%drive_letter%) do ( 
+for /f usebackq^tokens^=1-3*delims^=^" %%i in =;(
+    `type "%~f0" ^| findstr /b \^"`
+		);= do if exist "%%A:%%~k\%commons%\%specific%" ( echo\ && echo Exists - "%%A:%%~k\%commons%\%specific%"
+			if exist "%%A:%%~k\%commons%\%specific%\bak" (
+				echo\
+				for %%B in ("%%A:%%~k\%commons%\%specific%\*") do if not exist "%%A:%%~k\%commons%\%specific%\%%~nB" ( echo Creating a blank file - %%~nB && break>"%%A:%%~k\%commons%\%specific%\%%~nB" ) else ( echo Exists - "%%A:%%~k\%commons%\%specific%\%%~nB" )
+				echo\
+				for %%C in (%ext%) do if exist "%%A:%%~k\%commons%\%specific%\*.%%C" ( echo Processing all %%C files in "%%A:%%~k\%commons%\%specific%" && echo\ && dir "%%A:%%~k\%commons%\%specific%\*.%%C" && echo\ & echo\ && echo Moving the following to "%%A:%%~k\%commons%\%specific%\bak": && echo\ && @move "%%A:%%~k\%commons%\%specific%\*.%%C" "%%A:%%~k\%commons%\%specific%\bak" && echo\ ) else ( echo No file with %%C extension exists in "%%A:%%~k\%commons%\%specific%" )
+			echo\
+			) else ( md "%%A:%%~k\%commons%\%specific%\bak" && goto :process )
+			if exist "%%A:%%~k\%commons%\%ini%" ( echo Exists and editing - "%%A:%%~k\%commons%\%ini%" && echo\ && start "" /wait /min powershell.exe -NoP -Ex -By -c "(Get-Content -Path '%%A:%%~k\%commons%\%ini%') | ForEach-Object { $_.Replace(';+StartupMovies=StartupUE4','').Replace(';+StartupMovies=StartupNvidia','').Replace(';+StartupMovies=CinematicIntroV2','').Replace(';-StartupMovies=StartupUE4','').Replace(';-StartupMovies=StartupNvidia','').Replace(';-StartupMovies=CinematicIntroV2','').Replace('; -StartupMovies=StartupUE4','').Replace('; -StartupMovies=StartupNvidia','').Replace('; -StartupMovies=CinematicIntroV2','').Replace('; +StartupMovies=StartupUE4','').Replace('; +StartupMovies=StartupNvidia','').Replace('; +StartupMovies=CinematicIntroV2','').Replace('+StartupMovies=StartupUE4','').Replace('+StartupMovies=StartupNvidia','').Replace('+StartupMovies=CinematicIntroV2','').Replace('-StartupMovies=StartupUE4','').Replace('-StartupMovies=StartupNvidia','').Replace('-StartupMovies=CinematicIntroV2','').Replace('bWaitForMoviesToComplete=True','bWaitForMoviesToComplete=False').Replace('bMoviesAreSkippable=False','bMoviesAreSkippable=True').Replace('-StartupMovies=','').Replace(';-StartupMovies=','').Replace('; -StartupMovies=','') } | Set-Content -Path '%%A:%%~k\%commons%\%ini%'" 
+			) else ( echo Does not exist - "%%A:%%~k\%commons%\%ini%" )
+	) else ( echo Nonexistent - "%%A:%%~k\%commons%\%specific%" )
 )
-
-echo\ The end.
+echo\ && echo The end.
 pause>nul
 exit /b 0
+
+::--------------------------::
+::         DirList          ::
+::--------------------------::
+"1" "\Program Files (x86)\Steam"
+"2" "\Steam"
+"3" "\Games"
+"4" "\SteamLibrary"
